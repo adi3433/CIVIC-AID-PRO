ea2c5404de071a08925ab70e5d50597483342a6e
Lestlin Robins <lestlinr@gmail.com>
2026-01-17 15:57:26 +0530

    Adde login aand auth

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,8 +1,11 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://rlvgephkagtejlogudqo.supabase.co";
-const supabaseAnonKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJsdmdlcGhrYWd0ZWpsb2d1ZHFvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njg2MzI0MzgsImV4cCI6MjA4NDIwODQzOH0.mZs84-tEUKc73j0HqCXwaD1FDB-8C6fvvnPRuhPy2oM";
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+
+if (!supabaseUrl || !supabaseAnonKey) {
+  throw new Error("Missing Supabase environment variables");
+}
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
